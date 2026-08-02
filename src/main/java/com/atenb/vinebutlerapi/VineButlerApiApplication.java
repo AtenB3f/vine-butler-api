162030dc7d5277a398e233b5482a1065191f696b
AtenB <atenb3f@gmail.com>
2026-08-02 21:40:36 +0900

    first commit

---
+++ src/main/java/com/atenb/vinebutlerapi/VineButlerApiApplication.java
@@ -0,0 +1,13 @@
+package com.atenb.vinebutlerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VineButlerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VineButlerApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/atenb/vinebutlerapi/VineButlerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atenb.vinebutlerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VineButlerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
